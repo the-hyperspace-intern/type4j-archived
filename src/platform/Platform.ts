01df6b152f743b4dd45ecf0ff2b0b015f4a50bc3
Ginkoe <ginkoe@protonmail.com>
2020-11-07 04:22:02 +0100

    add string randomizer

--- src/platform/Platform.ts
+++ src/platform/Platform.ts
@@ -1,5 +1,13 @@
 export class PlatformUtils {
-    static getGlobalVariable(): any {
-        return global;
-    }
+  static getGlobalVariable(): any {
+    return global;
+  }
+
+  static randomIndice(length: number): string {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+    for (var i = length; i > 0; --i)
+      result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+  }
 }
