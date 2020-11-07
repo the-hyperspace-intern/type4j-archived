ff6fa7acfae9edddb546a2b5b235d197f39562e8
Ginkoe <ginkoe@protonmail.com>
2020-11-07 04:22:40 +0100

    ability to save entity from BaseNodeEntity WIP

--- src/repository/BaseNodeEntity.ts
+++ src/repository/BaseNodeEntity.ts
@@ -1,5 +1,29 @@
+import { stringify } from "querystring";
+import { getConnection } from "..";
+import { QueryBuilder } from "../query/QueryBuilder";
+
+export function _findProps(entity: BaseNodeEntity): any {
+  const props = {};
+  Object.keys(entity).forEach((prop) => {
+    if (Reflect.getMetadata("isGraphProp", entity, prop))
+      props[prop] = entity[prop];
+  });
+  return props;
+}
+
 export class BaseNodeEntity {
-  save() {
+  _getNodeType() {
+    //TODO: Custom Name
+    const _reflectedName = Reflect.getMetadata("4jEntityName", this);
+    const _className = (<any>this).constructor.name;
+    return _reflectedName ?? _className;
+  }
+
+  async save(): Promise<this> {
+    const queryBuilder = new QueryBuilder();
+    queryBuilder.create(this);
+    await queryBuilder.run(getConnection());
+
     return this;
   }
 }
