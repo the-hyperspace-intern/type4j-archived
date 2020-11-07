98e8c73ea694f1c6a036008c655e5780a5fba20d
Ginkoe <ginkoe@protonmail.com>
2020-11-07 01:47:20 +0100

    Moving GraphProp Decorator

--- src/decorators/GraphProp.ts
+++ src/decorators/GraphProp.ts
@@ -1,4 +1,4 @@
 // Prop Decorator
 export const GraphProp = () => (target: any, propKey: string) => {
-    Reflect.defineMetadata('isGraphProp', true, target, propKey);
+  Reflect.defineMetadata("isGraphProp", true, target, propKey);
 };
